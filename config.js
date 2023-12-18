627c2189c0ee99915c11a3a737c77c9e06c957d5
adiogentgit <153903098+adiogentgit@users.noreply.github.com>
2023-12-18 15:35:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // export const baseUrl = "https://dochomoeo-backend.onrender.com";
 // export const baseUrl = "https://shark-app-neruo.ondigitalocean.app";
-export const baseUrl = "http://localhost:10000";
-// export const baseUrl = "https://coral-app-bxh4h.ondigitalocean.app";
+// export const baseUrl = "http://localhost:10000";
+export const baseUrl = "https://coral-app-bxh4h.ondigitalocean.app";
 
  
